472083695620d2bcad450bced446764b0650807f
Adem <ademduran6888@gmail.com>
2024-02-02 06:28:02 +0100

    feat(storybook): setting backgrounds and layout

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,16 @@
 import type { Preview } from "@storybook/react";
+import '../src/index.css'; 
 
 const preview: Preview = {
   parameters: {
+    layout: "centered",
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#EEEEEE" },
+        { name: "dark", value: "#131313" },
+      ],
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
