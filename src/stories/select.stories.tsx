d4569334c2d946a3c0288866df2b97c4d471e4d3
Adem <ademduran6888@gmail.com>
2024-02-02 15:04:46 +0100

    fix(storybook): correcting select items values

--- src/stories/select.stories.tsx
+++ src/stories/select.stories.tsx
@@ -29,11 +29,11 @@ export const Disabled = () => (
       <SelectValue placeholder="Select a theme for your website" />
     </SelectTrigger>
     <SelectContent>
-      <SelectItem value="apple">Umbrella UI</SelectItem>
-      <SelectItem value="banana">Umbrella Light</SelectItem>
-      <SelectItem value="blueberry">Dark Umbrella</SelectItem>
-      <SelectItem value="grapes">Umbrella Reverse</SelectItem>
-      <SelectItem value="pineapple">S.T.A.R.S.</SelectItem>
+      <SelectItem value="umbrella-ui">Umbrella UI</SelectItem>
+      <SelectItem value="umbrella-light">Umbrella Light</SelectItem>
+      <SelectItem value="dark-umbrella">Dark Umbrella</SelectItem>
+      <SelectItem value="umbrella-reverse">Umbrella Reverse</SelectItem>
+      <SelectItem value="stars">S.T.A.R.S.</SelectItem>
     </SelectContent>
   </Select>
 );
