6b9e7d1777dc977231fde558c2d013a289bb452c
Adem <ademduran6888@gmail.com>
2024-02-05 20:59:48 +0100

    feat(storybook): adding the draggable item component's stories

--- src/stories/draggable.stories.tsx
+++ src/stories/draggable.stories.tsx
@@ -4,6 +4,12 @@ export default { title: 'Draggable' };
 
 export const Basic = () => (
   <div className="flex justify-center items-center gap-4">
-    <DraggableItem />
+    <DraggableItem itemName="menu">Menu</DraggableItem>
+  </div>
+);
+
+export const Disabled = () => (
+  <div className="flex justify-center items-center gap-4">
+    <DraggableItem itemName="menu" disabled>Menu</DraggableItem>
   </div>
 );
