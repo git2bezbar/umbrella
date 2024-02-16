ac09a39c286e4b4c51f28ce58f19a39260869c32
Adem <ademduran6888@gmail.com>
2024-02-16 15:19:11 +0100

    chore: adding reactRefresh()

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import reactRefresh from "@vitejs/plugin-react-refresh"
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
