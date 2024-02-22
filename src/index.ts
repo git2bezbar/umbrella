77fda6e366934372a57df2ff18701d9efab06a46
Adem <ademduran6888@gmail.com>
2024-02-22 09:35:49 +0100

    fix(index): fixing imports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-export { Button, buttonVariants, type ButtonProps } from '@/components/ui/button'
-export { Color, type ColorProps } from '@/components/ui/color'
-export { DraggableItem, type DraggableItemProps } from '@/components/ui/draggable'
-export { Input, type InputProps } from '@/components/ui/input'
-export { Label } from '@/components/ui/label'
-export { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+export { Button, buttonVariants, type ButtonProps } from './components/ui/button'
+export { Color, type ColorProps } from './components/ui/color'
+export { DraggableItem, type DraggableItemProps } from './components/ui/draggable'
+export { Input, type InputProps } from './components/ui/input'
+export { Label } from './components/ui/label'
+export { RadioGroup, RadioGroupItem } from './components/ui/radio-group'
 export { 
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
-export { Switch } from '@/components/ui/switch'
+} from './components/ui/select'
+export { Switch } from './components/ui/switch'
 export {
   Table,
   TableBody,
@@ -19,6 +19,6 @@ export {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table'
+} from './components/ui/table'
 
-export * from '@/lib/utils'
+export * from './lib/utils'
